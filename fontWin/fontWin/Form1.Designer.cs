7a49a0e7a7adbd5b13bff25b55006db178764cd8
ofserendipity <124275097+ofserendipity@users.noreply.github.com>
2023-02-02 22:26:40 +0800

    Add files via upload

---
+++ fontWin/fontWin/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace fontWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MistyRose;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(165, 280);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "更多";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_MouseClick);
+            this.button2.MouseHover += new System.EventHandler(this.button2_MouseHover);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Moccasin;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(304, 280);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 34);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "播放";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PeachPuff;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(13, 280);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开单词本";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.MistyRose;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("楷体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Items.AddRange(new object[] {
+            "退出",
+            "修改字号-大",
+            "修改字号-中",
+            "修改字号-小",
+            "置顶/取消置顶",
+            "修改颜色-粉",
+            "修改颜色-绿",
+            "修改颜色-蓝"});
+            this.listBox1.Location = new System.Drawing.Point(196, 290);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(154, 168);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.Visible = false;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1737, 460);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ fontWin/fontWin/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fontWin
+{
+    public partial class Form1 : Form
+    {
+        string wordlistpath = string.Empty;
+        List<string> wordlist = new List<string>();
+        private Point mouseoff;//鼠标移动位置变量
+        private bool leftflag;//鼠标是否为左键
+        const int MaxFontSize = 70;
+        const int MidFontSize = 50;
+        const int MinFontSize = 30;
+        private int FontSize = MaxFontSize;
+        private Color FontColor= Color.Orchid;
+        string newword = "Precipitous";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 3500; 
+        }
+        
+
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "请选择txt单词本，一个单词占一行";
+            openFileDialog.Filter = "文本文件（*.txt)|*.txt|所有文件（*.)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                wordlistpath = openFileDialog.FileName.ToString();
+
+            }
+            StreamReader sr = new StreamReader(wordlistpath, Encoding.Default);
+            string line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                wordlist.Add(line);
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            int index = rd.Next(wordlist.Count());
+            if(index>0) newword = wordlist[index];
+            this.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (wordlistpath == string.Empty) MessageBox.Show("请先输入单词本");
+            else 
+            {
+                timer1.Enabled = !timer1.Enabled;
+                if (timer1.Enabled == true)
+                {
+                    button3.Text = "暂停";
+                }
+                else
+                {
+                    button3.Text = "播放";
+                }                        
+            }
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            SolidBrush sldBrush = new SolidBrush(FontColor);
+            Font font = new Font("楷体", FontSize, FontStyle.Bold);
+            newword = newword.ToString();
+            g.DrawString(newword, font, sldBrush, new Point(20, 20));
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                mouseoff = new Point(-e.X, -e.Y);
+                leftflag = true;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(leftflag)
+            {
+                Point mouseset = Control.MousePosition;
+                mouseset.Offset(mouseoff.X, mouseoff.Y);
+                Location = mouseset;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(leftflag)
+            {
+                leftflag = false;
+            }
+        }
+        
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           switch(listBox1.SelectedIndex)
+            {
+                case 0: this.Close();break;
+                case 1: FontSize = MaxFontSize;this.Refresh(); break;
+                case 2:FontSize = MidFontSize; this.Refresh(); break;
+                case 3: FontSize = MinFontSize;this.Refresh();break;
+                case 4: this.TopMost = !this.TopMost; break;
+                case 5: FontColor = Color.Orchid;this.Refresh();break;
+                case 6: FontColor = Color.Green;this.Refresh();break;
+                case 7:FontColor = Color.Blue;this.Refresh();break;
+                
+
+            }
+
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            listBox1.Visible = true;
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (listBox1.Visible == true)
+            {
+                listBox1.Visible = false;
+            }
+        }
+    }
+}
